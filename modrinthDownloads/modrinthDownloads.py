c21c31d9eb7f0e7e08e0674b7d119d09f0fae161
thecoolguy62aws <150736455+thecoolguy62aws@users.noreply.github.com>
2026-04-27 13:05:52 -0700

    fix: fix bug where numbers would get printed on the exit prompt

--- modrinthDownloads/modrinthDownloads.py
+++ modrinthDownloads/modrinthDownloads.py
@@ -67,22 +67,15 @@ def print_center_top(win, text):
     win.addstr(y, x, text)
     win.refresh()
 
-def quit_thread_task(scr):
-    height, width = scr.getmaxyx()
-    message = "Press Q to quit"
-    message_y = height - 1
-    message_x = width - len(message)
-    while True:
-        try:
-            scr.addstr(message_y, message_x, message)
-        except curses.error:
-            pass
-        scr.refresh()
-        if scr.getch() == ord('q'):
-            curses.endwin()
-            os._exit(0)
+def request_thread_task(username):
+    global response
+            
+    response = requests.get(f"https://api.modrinth.com/v2/user/{username}/projects", headers=headers)
 
 def main(stdscr):
+    global response
+    response = None
+    
     stdscr.nodelay(True)
     stdscr.clear()
     
@@ -95,20 +88,38 @@ def main(stdscr):
     stdscr.clear()
     stdscr.refresh()
     
-    quit_thread = threading.Thread(target=quit_thread_task, args=(stdscr,), daemon=True)
-    quit_thread.start()
+    last_update_time = time.perf_counter() - 10
     
     while True:
-        response = requests.get(f"https://api.modrinth.com/v2/user/{username}/projects", headers=headers)
-        if response.status_code == 200:
-            response_data = response.json()
-            
-            downloads = 0
-            for item in response_data:
-                downloads += item["downloads"]
+        height, width = stdscr.getmaxyx()
+        message = "Press Q to quit"
+        message_y = height - 1
+        message_x = width - len(message)
+        try:
+            stdscr.addstr(message_y, message_x, message)
+        except curses.error:
+            pass
+        stdscr.refresh()
+        if stdscr.getch() == ord('q'):
+            curses.endwin()
+            os._exit(0)
+        
+        current_time = time.perf_counter()
+        
+        if response != None:
+            if response.status_code == 200:
+                response_data = response.json()
+                
+                downloads = 0
+                for item in response_data:
+                    downloads += item["downloads"]
 
-            print_center_top(stdscr, f"Viewing: {username}")
-            print_center_one_up(stdscr, f"Live Downloads:")
-            print_bold_center(stdscr, f"{downloads:,}")
-            stdscr.refresh()
-        time.sleep(10)
+                print_center_top(stdscr, f"Viewing: {username}")
+                print_center_one_up(stdscr, f"Live Downloads:")
+                print_bold_center(stdscr, f"{downloads:,}")
+                stdscr.refresh()
+            response = None
+        if (current_time - last_update_time) >= 10:
+            new_thread = threading.Thread(target=request_thread_task, args=(username,), daemon=True)
+            new_thread.start()
+            last_update_time = current_time
